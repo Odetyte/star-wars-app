5b5b4bad8fc3d314ec2391e55cfdd98c62d68ff6
Odeta Maslovaite <odetamaslovaite@yahoo.com>
2020-12-21 14:30:36 +0100

    React App Setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div>
+    <p>Star Wars App in progress 🔧</p>
+  </div>
+)
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './App.css'
+import './index.css'
+
+const App = () => (
+  <div>
+    <p>Star Wars App in progress 🔧</p>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
